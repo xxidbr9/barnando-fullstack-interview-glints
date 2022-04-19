9df59716b52431f2337fa473698bd313ac07de7b
Barnando Akbarto Hidayatullah <barnando13@gmail.com>
2022-04-20 02:11:37 +0700

    ci: test ci action

--- packages/backend/src/shared/helpers/socialOAuth2.helper.ts
+++ packages/backend/src/shared/helpers/socialOAuth2.helper.ts
@@ -1,22 +1,6 @@
 import { IOAuthProvider, IOAuth2Resp, IFacebookResp, IGoogleResp } from "@shared/types/accountSocial";
 import axios from "axios";
 
-async function getUserSocialInfoFromProvider(accessToken: string, provider: IOAuthProvider): Promise<IOAuth2Resp> {
-  switch (provider) {
-    case "facebook":
-      return await getFromFacebook(accessToken)
-    case "google":
-      return await getFromGoogle(accessToken)
-    case "google-one-tap":
-      return await getFromGoogleOneTap(accessToken)
-
-    default:
-      throw Error("social provider are not valid")
-  }
-}
-
-export default getUserSocialInfoFromProvider
-
 // Get from facebook
 async function getFromFacebook(accessToken: string): Promise<IOAuth2Resp> {
   const resp = await axios.get(`https://graph.facebook.com/me?access_token=${accessToken}&fields=id,name,email,picture.width(720).height(720)`)
@@ -58,3 +42,19 @@ async function getFromGoogleOneTap(accessToken: string): Promise<IOAuth2Resp> {
   }
 
 }
+
+async function getUserSocialInfoFromProvider(accessToken: string, provider: IOAuthProvider): Promise<IOAuth2Resp> {
+  switch (provider) {
+    case "facebook":
+      return await getFromFacebook(accessToken)
+    case "google":
+      return await getFromGoogle(accessToken)
+    case "google-one-tap":
+      return await getFromGoogleOneTap(accessToken)
+
+    default:
+      throw Error("social provider are not valid")
+  }
+}
+
+export default getUserSocialInfoFromProvider
